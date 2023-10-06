5a7be8dbf92027dad5648dfd8a943fb502ca821c
Kartik Singh <kartik.ynwa@gmail.com>
2023-10-06 11:05:53 +0530

    Add emacs configuration
    Based on ~ashton314/emacs-bedrock. Lots of work remaining. :)

---
+++ dotfiles/dot_config/emacs/README.md
@@ -0,0 +1,5 @@
+# Based on Emacs Bedrock
+
+Stepping stones to a better Emacs experience
+
+Built using: [~ashton314/emacs-bedrock](https://git.sr.ht/~ashton314/emacs-bedrock)
---
+++ dotfiles/dot_config/emacs/extras/dev.el
@@ -0,0 +1,82 @@
+;;; Emacs Bedrock
+;;;
+;;; Extra config: Development tools
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Version Control
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Magit: best Git client to ever exist
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Common file types
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package markdown-mode
+  :commands (markdown-mode)
+  :hook ((markdown-mode . visual-line-mode)))
+
+(use-package yaml-mode
+  :commands (yaml-mode)
+  :ensure t)
+
+(use-package json-mode
+  :commands (json-mode)
+  :ensure t)
+
+(use-package web-mode
+  :mode
+  "\\.html\\'"
+  "\\.html.erb\\'"
+  "\\.phtml\\'"
+  "\\.tpl\\.php\\'"
+  "\\.[agj]sp\\'"
+  "\\.as[cp]x\\'"
+  "\\.erb\\'"
+  "\\.mustache\\'"
+  "\\.djhtml\\'"
+  "\\.jsx?\\'"
+
+  :custom
+  (web-mode-code-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-markup-indent-offset 2)
+  (web-mode-attr-indent-offset 2)
+  (web-mode-sql-indent-offset 2)
+
+  :commands (web-mode))
+
+;; Emacs ships with a lot of popular programming language modes. If it's not
+;; built in, you're almost certain to find a mode for the language you're
+;; looking for with a quick Internet search.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Eglot, the built-in LSP client for Emacs
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package eglot
+  :elpaca nil
+
+  :custom
+  (eglot-send-changes-idle-time 0.1)
+
+  :config
+  (fset #'jsonrpc--log-event #'ignore))  ; massive perf boost---don't log every event
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Utilities
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package projectile :config (projectile-mode +1))
