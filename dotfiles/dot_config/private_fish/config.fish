8404954e89f875c75f722065de742c550bbf0206
Kartik Singh <kartik.ynwa@gmail.com>
2025-03-26 08:56:39 +0530

    fish: Add configuration

---
+++ dotfiles/dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+abbr -a xu sudo xbps-install -Su
+abbr -a xuy sudo xbps-install -Suy
+abbr -a xs xbps-query -Rs
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
